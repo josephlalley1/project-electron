35b4b76632c0b08fa3ff3305c7901b47840726b6
Joseph Lalley <josephyzyr@gmail.com>
2019-03-06 10:09:09 +0000

    frequent changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow} = require('electron');
 const url = require('url');
 
 function boot() {
-  let win = new BrowserWindow({width: 1012, height: 600, frame: true})
+  let win = new BrowserWindow({width: 1012, height:600, minWidth: 1012, minHeight: 600, frame: true})
   win.loadURL('file://' + __dirname + '/index.html');﻿
 }
 
